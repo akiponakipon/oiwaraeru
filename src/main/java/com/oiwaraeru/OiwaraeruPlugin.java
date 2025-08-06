24e7affc73515ffeabfd0905dd072cb919d5c4e1
akiponakipon <ojiakaoldriver@gmail.com>
2025-08-06 09:37:28 +0900

    Update OiwaraeruPlugin.java

--- src/main/java/com/oiwaraeru/OiwaraeruPlugin.java
+++ src/main/java/com/oiwaraeru/OiwaraeruPlugin.java
@@ -1,143 +1,126 @@
-package com.oiwaraeru;
+    package com.oiwaraeru;
 
-import org.bukkit.*;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.*;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.inventory.CraftItemEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.*;
-import org.bukkit.inventory.meta.ItemMeta;
+import com.oiwaraeru.listeners.ItemListener;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.*;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
 
-import java.util.*;
-
-public class OiwaraeruPlugin extends JavaPlugin implements Listener {
-
-    private final Map<UUID, Set<String>> craftedItems = new HashMap<>();
-    private final Map<UUID, Long> iceRodCooldown = new HashMap<>();
-    private final Map<UUID, Long> fireRodCooldown = new HashMap<>();
-    private final Map<UUID, Long> legendaryBowChargeStart = new HashMap<>();
-    private final Set<UUID> chargingPlayers = new HashSet<>();
-
-    private static final String VAMPIRE_SWORD_NAME = ChatColor.DARK_RED + "吸血鬼の剣";
-    private static final String ICE_ROD_NAME = ChatColor.AQUA + "氷の杖";
-    private static final String FIRE_ROD_NAME = ChatColor.RED + "炎の杖";
-    private static final String LEGENDARY_BOW_NAME = ChatColor.GOLD + "伝説の弓";
-    private static final String LEGENDARY_SWORD_NAME = ChatColor.DARK_PURPLE + "伝説の剣";
+public class OiwaraeruPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getLogger().info("Oiwaraeru Plugin Enabled");
-        getServer().getPluginManager().registerEvents(this, this);
+        Bukkit.getPluginManager().registerEvents(new ItemListener(this), this);
         registerRecipes();
     }
 
     private void registerRecipes() {
-        // 吸血鬼の剣
-        ItemStack vampireSword = createVampireSword();
+        // 吸血鬼の剣レシピ
+        ItemStack vampireSword = ItemListener.createVampireSword();
         ShapedRecipe vampireRecipe = new ShapedRecipe(new NamespacedKey(this, "vampire_sword"), vampireSword);
         vampireRecipe.shape("  m", "epe", "isb");
-        vampireRecipe.setIngredient('m', Material.ROTTEN_FLESH);
-        vampireRecipe.setIngredient('e', Material.ENDER_EYE);
-        vampireRecipe.setIngredient('p', Material.STICK);
-        vampireRecipe.setIngredient('i', Material.IRON_SWORD);
-        vampireRecipe.setIngredient('s', Material.BLAZE_POWDER);
-        vampireRecipe.setIngredient('b', Material.BLAZE_POWDER);
+        vampireRecipe.setIngredient('m', org.bukkit.Material.ROTTEN_FLESH);
+        vampireRecipe.setIngredient('e', org.bukkit.Material.ENDER_EYE);
+        vampireRecipe.setIngredient('p', org.bukkit.Material.STICK);
+        vampireRecipe.setIngredient('i', org.bukkit.Material.IRON_SWORD);
+        vampireRecipe.setIngredient('s', org.bukkit.Material.BLAZE_POWDER);
+        vampireRecipe.setIngredient('b', org.bukkit.Material.BLAZE_POWDER);
         Bukkit.addRecipe(vampireRecipe);
 
-        // 氷の杖
-        ItemStack iceRod = createIceRod();
+        // 氷の杖レシピ
+        ItemStack iceRod = ItemListener.createIceRod();
         ShapedRecipe iceRodRecipe = new ShapedRecipe(new NamespacedKey(this, "ice_rod"), iceRod);
         iceRodRecipe.shape("  i", " s ", "   ");
-        iceRodRecipe.setIngredient('i', Material.ICE);
-        iceRodRecipe.setIngredient('s', Material.SNOW_BLOCK);
+        iceRodRecipe.setIngredient('i', org.bukkit.Material.ICE);
+        iceRodRecipe.setIngredient('s', org.bukkit.Material.SNOW_BLOCK);
         Bukkit.addRecipe(iceRodRecipe);
 
-        // 炎の杖
-        ItemStack fireRod = createFireRod();
+        // 炎の杖レシピ
+        ItemStack fireRod = ItemListener.createFireRod();
         ShapedRecipe fireRodRecipe = new ShapedRecipe(new NamespacedKey(this, "fire_rod"), fireRod);
         fireRodRecipe.shape("  s", " m ", "bs ");
-        fireRodRecipe.setIngredient('s', Material.BLAZE_POWDER);
-        fireRodRecipe.setIngredient('m', Material.MAGMA_BLOCK);
-        fireRodRecipe.setIngredient('b', Material.STICK);
+        fireRodRecipe.setIngredient('s', org.bukkit.Material.BLAZE_POWDER);
+        fireRodRecipe.setIngredient('m', org.bukkit.Material.MAGMA_BLOCK);
+        fireRodRecipe.setIngredient('b', org.bukkit.Material.STICK);
         Bukkit.addRecipe(fireRodRecipe);
 
-        // 伝説の弓
-        ItemStack legendaryBow = createLegendaryBow();
+        // 伝説の弓レシピ
+        ItemStack legendaryBow = ItemListener.createLegendaryBow();
         ShapedRecipe legendaryBowRecipe = new ShapedRecipe(new NamespacedKey(this, "legendary_bow"), legendaryBow);
         legendaryBowRecipe.shape(" p ", "aya", " f ");
-        legendaryBowRecipe.setIngredient('p', Material.PAPER);
-        legendaryBowRecipe.setIngredient('a', Material.ARROW);
-        legendaryBowRecipe.setIngredient('y', Material.BOW);
-        legendaryBowRecipe.setIngredient('f', Material.FLINT_AND_STEEL);
+        legendaryBowRecipe.setIngredient('p', org.bukkit.Material.PAPER);
+        legendaryBowRecipe.setIngredient('a', org.bukkit.Material.ARROW);
+        legendaryBowRecipe.setIngredient('y', org.bukkit.Material.BOW);
+        legendaryBowRecipe.setIngredient('f', org.bukkit.Material.FLINT_AND_STEEL);
         Bukkit.addRecipe(legendaryBowRecipe);
 
-        // 伝説の剣
-        ItemStack legendarySword = createLegendarySword();
+        // 伝説の剣レシピ
+        ItemStack legendarySword = ItemListener.createLegendarySword();
         ShapedRecipe legendarySwordRecipe = new ShapedRecipe(new NamespacedKey(this, "legendary_sword"), legendarySword);
         legendarySwordRecipe.shape(" n ", "sss", " s ");
-        legendarySwordRecipe.setIngredient('n', Material.NETHERITE_SWORD);
-        legendarySwordRecipe.setIngredient('s', Material.GOLD_INGOT);
+        legendarySwordRecipe.setIngredient('n', org.bukkit.Material.NETHERITE_SWORD);
+        legendarySwordRecipe.setIngredient('s', org.bukkit.Material.GOLD_INGOT);
         Bukkit.addRecipe(legendarySwordRecipe);
+
+        // 最高の食料レシピ
+        ItemStack bestFood = ItemListener.createBestFood();
+        ShapedRecipe bestFoodRecipe = new ShapedRecipe(new NamespacedKey(this, "best_food"), bestFood);
+        bestFoodRecipe.shape(" s ", "sgs", " s ");
+        bestFoodRecipe.setIngredient('s', org.bukkit.Material.COOKED_BEEF);
+        bestFoodRecipe.setIngredient('g', org.bukkit.Material.GOLDEN_CARROT);
+        Bukkit.addRecipe(bestFoodRecipe);
+
+        // 伝説のクロスボウレシピ
+        ItemStack legendaryCrossbow = ItemListener.createLegendaryCrossbow();
+        ShapedRecipe legendaryCrossbowRecipe = new ShapedRecipe(new NamespacedKey(this, "legendary_crossbow"), legendaryCrossbow);
+        legendaryCrossbowRecipe.shape(" a ", " c ", " b ");
+        legendaryCrossbowRecipe.setIngredient('a', org.bukkit.Material.ARROW);
+        legendaryCrossbowRecipe.setIngredient('c', org.bukkit.Material.CROSSBOW);
+        legendaryCrossbowRecipe.setIngredient('b', org.bukkit.Material.BLAZE_POWDER);
+        Bukkit.addRecipe(legendaryCrossbowRecipe);
+
+        // なんでも屋さんレシピ
+        ItemStack handyman = ItemListener.createHandyman();
+        ShapedRecipe handymanRecipe = new ShapedRecipe(new NamespacedKey(this, "handyman"), handyman);
+        handymanRecipe.shape("   ", " e ", "   ");
+        handymanRecipe.setIngredient('e', org.bukkit.Material.EMERALD_BLOCK);
+        Bukkit.addRecipe(handymanRecipe);
+
+        // 壊れたエンダーパールレシピ
+        ItemStack brokenEnderPearl = ItemListener.createBrokenEnderPearl();
+        ShapedRecipe brokenEnderPearlRecipe = new ShapedRecipe(new NamespacedKey(this, "broken_ender_pearl"), brokenEnderPearl);
+        brokenEnderPearlRecipe.shape("   ", " e ", "   ");
+        brokenEnderPearlRecipe.setIngredient('e', org.bukkit.Material.ENDER_PEARL);
+        Bukkit.addRecipe(brokenEnderPearlRecipe);
+
+        // 投げるスポンジレシピ
+        ItemStack throwingSponge = ItemListener.createThrowingSponge();
+        ShapedRecipe throwingSpongeRecipe = new ShapedRecipe(new NamespacedKey(this, "throwing_sponge"), throwingSponge);
+        throwingSpongeRecipe.shape("   ", " s ", "   ");
+        throwingSpongeRecipe.setIngredient('s', org.bukkit.Material.SPONGE);
+        Bukkit.addRecipe(throwingSpongeRecipe);
+
+        // 超強力TNTo-6型b式レシピ
+        ItemStack superTNT = ItemListener.createSuperTNT();
+        ShapedRecipe superTNTRecipe = new ShapedRecipe(new NamespacedKey(this, "super_tnt"), superTNT);
+        superTNTRecipe.shape("   ", " t ", "   ");
+        superTNTRecipe.setIngredient('t', org.bukkit.Material.TNT);
+        Bukkit.addRecipe(superTNTRecipe);
+
+        // 暗黒の剣レシピ
+        ItemStack darkSword = ItemListener.createDarkSword();
+        ShapedRecipe darkSwordRecipe = new ShapedRecipe(new NamespacedKey(this, "dark_sword"), darkSword);
+        darkSwordRecipe.shape(" n ", " n ", " n ");
+        darkSwordRecipe.setIngredient('n', org.bukkit.Material.NETHERITE_SWORD);
+        Bukkit.addRecipe(darkSwordRecipe);
+
+        // 移動不可の弓レシピ
+        ItemStack immobileBow = ItemListener.createImmobileBow();
+        ShapedRecipe immobileBowRecipe = new ShapedRecipe(new NamespacedKey(this, "immobile_bow"), immobileBow);
+        immobileBowRecipe.shape(" b ", " b ", " b ");
+        immobileBowRecipe.setIngredient('b', org.bukkit.Material.BOW);
+        Bukkit.addRecipe(immobileBowRecipe);
     }
-
-    private ItemStack createVampireSword() {
-        ItemStack item = new ItemStack(Material.IRON_SWORD);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(VAMPIRE_SWORD_NAME);
-        meta.setLore(Collections.singletonList(ChatColor.RED + "攻撃ダメージの50%を吸収する"));
-        item.setItemMeta(meta);
-        return item;
-    }
-
-    private ItemStack createIceRod() {
-        ItemStack item = new ItemStack(Material.BRINE_ROD);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ICE_ROD_NAME);
-        meta.setLore(Collections.singletonList(ChatColor.BLUE + "ブロックリーチ50m 移動速度低下付与"));
-        item.setItemMeta(meta);
-        return item;
-    }
-
-    private ItemStack createFireRod() {
-        ItemStack item = new ItemStack(Material.BLAZE_ROD);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(FIRE_ROD_NAME);
-        meta.setLore(Collections.singletonList(ChatColor.GOLD + "周囲mobに炎上付与"));
-        item.setItemMeta(meta);
-        return item;
-    }
-
-    private ItemStack createLegendaryBow() {
-        ItemStack item = new ItemStack(Material.BOW);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(LEGENDARY_BOW_NAME);
-        meta.setLore(Collections.singletonList(ChatColor.YELLOW + "チャージ最大750ダメージ"));
-        item.setItemMeta(meta);
-        return item;
-    }
-
-    private ItemStack createLegendarySword() {
-        ItemStack item = new ItemStack(Material.NETHERITE_SWORD);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(LEGENDARY_SWORD_NAME);
-        meta.setLore(Arrays.asList(
-            ChatColor.GREEN + "移動速度上昇5",
-            ChatColor.GREEN + "攻撃力上昇3",
-            ChatColor.GREEN + "ジャンプ力上昇2",
-            ChatColor.GREEN + "回復速度上昇5"
-        ));
-        item.setItemMeta(meta);
-        return item;
-    }
-
-    // ここから実際の効果処理（イベントハンドラなど）を追加していきます...
-
 }
