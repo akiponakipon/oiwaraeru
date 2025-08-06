7b58e7d07087d792736edd6e837c51dfac5fbd65
akiponakipon <ojiakaoldriver@gmail.com>
2025-08-06 09:30:47 +0900

    Update OiwaraeruPlugin.java

--- src/main/java/com/oiwaraeru/OiwaraeruPlugin.java
+++ src/main/java/com/oiwaraeru/OiwaraeruPlugin.java
@@ -1,20 +1,12 @@
 package com.oiwaraeru;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.Particle;
-import org.bukkit.Server;
-import org.bukkit.Sound;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityResurrectEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.*;
@@ -25,72 +17,61 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class OiwaraeruPlugin extends JavaPlugin implements Listener {
 
     private final Map<UUID, Set<String>> craftedItems = new HashMap<>();
-    private final Map<UUID, Boolean> resurrectionFlag = new HashMap<>();
+    private final Map<UUID, Long> iceRodCooldown = new HashMap<>();
+    private final Map<UUID, Long> fireRodCooldown = new HashMap<>();
+    private final Map<UUID, Long> legendaryBowChargeStart = new HashMap<>();
+    private final Set<UUID> chargingPlayers = new HashSet<>();
 
-    // アイテムのレシピKey定義
-    private final NamespacedKey vampireSwordKey = new NamespacedKey(this, "vampire_sword");
-    private final NamespacedKey iceRodKey = new NamespacedKey(this, "ice_rod");
-    private final NamespacedKey fireRodKey = new NamespacedKey(this, "fire_rod");
-    private final NamespacedKey legendaryBowKey = new NamespacedKey(this, "legendary_bow");
-    private final NamespacedKey legendarySwordKey = new NamespacedKey(this, "legendary_sword");
-    private final NamespacedKey bestFoodKey = new NamespacedKey(this, "best_food");
-    private final NamespacedKey legendaryCrossbowKey = new NamespacedKey(this, "legendary_crossbow");
-    private final NamespacedKey handymanKey = new NamespacedKey(this, "handyman");
-    private final NamespacedKey brokenEnderPearlKey = new NamespacedKey(this, "broken_ender_pearl");
-    private final NamespacedKey throwableSpongeKey = new NamespacedKey(this, "throwable_sponge");
-    private final NamespacedKey superTntKey = new NamespacedKey(this, "super_tnt");
-    private final NamespacedKey darkSwordKey = new NamespacedKey(this, "dark_sword");
-    private final NamespacedKey immobilizeBowKey = new NamespacedKey(this, "immobilize_bow");
+    private static final String VAMPIRE_SWORD_NAME = ChatColor.DARK_RED + "吸血鬼の剣";
+    private static final String ICE_ROD_NAME = ChatColor.AQUA + "氷の杖";
+    private static final String FIRE_ROD_NAME = ChatColor.RED + "炎の杖";
+    private static final String LEGENDARY_BOW_NAME = ChatColor.GOLD + "伝説の弓";
+    private static final String LEGENDARY_SWORD_NAME = ChatColor.DARK_PURPLE + "伝説の剣";
 
     @Override
     public void onEnable() {
-        getLogger().info("Oiwaraeru Plugin enabled");
+        getLogger().info("Oiwaraeru Plugin Enabled");
         getServer().getPluginManager().registerEvents(this, this);
         registerRecipes();
     }
 
-    @Override
-    public void onDisable() {
-        getLogger().info("Oiwaraeru Plugin disabled");
-    }
-
     private void registerRecipes() {
         // 吸血鬼の剣
-        ShapedRecipe vampireSwordRecipe = new ShapedRecipe(vampireSwordKey, createVampireSword());
-        vampireSwordRecipe.shape("  m", "epe", "isb");
-        vampireSwordRecipe.setIngredient('m', Material.ROTTEN_FLESH); // 羊の生肉はないので腐った肉代用
-        vampireSwordRecipe.setIngredient('e', Material.ENDER_EYE);
-        vampireSwordRecipe.setIngredient('p', Material.STICK);
-        vampireSwordRecipe.setIngredient('i', Material.IRON_SWORD);
-        vampireSwordRecipe.setIngredient('s', Material.BLAZE_POWDER);
-        vampireSwordRecipe.setIngredient('b', Material.BLAZE_POWDER);
-        Bukkit.addRecipe(vampireSwordRecipe);
+        ItemStack vampireSword = createVampireSword();
+        ShapedRecipe vampireRecipe = new ShapedRecipe(new NamespacedKey(this, "vampire_sword"), vampireSword);
+        vampireRecipe.shape("  m", "epe", "isb");
+        vampireRecipe.setIngredient('m', Material.ROTTEN_FLESH);
+        vampireRecipe.setIngredient('e', Material.ENDER_EYE);
+        vampireRecipe.setIngredient('p', Material.STICK);
+        vampireRecipe.setIngredient('i', Material.IRON_SWORD);
+        vampireRecipe.setIngredient('s', Material.BLAZE_POWDER);
+        vampireRecipe.setIngredient('b', Material.BLAZE_POWDER);
+        Bukkit.addRecipe(vampireRecipe);
 
         // 氷の杖
-        ShapedRecipe iceRodRecipe = new ShapedRecipe(iceRodKey, createIceRod());
+        ItemStack iceRod = createIceRod();
+        ShapedRecipe iceRodRecipe = new ShapedRecipe(new NamespacedKey(this, "ice_rod"), iceRod);
         iceRodRecipe.shape("  i", " s ", "   ");
         iceRodRecipe.setIngredient('i', Material.ICE);
         iceRodRecipe.setIngredient('s', Material.SNOW_BLOCK);
-        iceRodRecipe.setIngredient(' ', Material.AIR);
-        iceRodRecipe.setIngredient(' ', Material.AIR);
         Bukkit.addRecipe(iceRodRecipe);
 
         // 炎の杖
-        ShapedRecipe fireRodRecipe = new ShapedRecipe(fireRodKey, createFireRod());
+        ItemStack fireRod = createFireRod();
+        ShapedRecipe fireRodRecipe = new ShapedRecipe(new NamespacedKey(this, "fire_rod"), fireRod);
         fireRodRecipe.shape("  s", " m ", "bs ");
         fireRodRecipe.setIngredient('s', Material.BLAZE_POWDER);
         fireRodRecipe.setIngredient('m', Material.MAGMA_BLOCK);
         fireRodRecipe.setIngredient('b', Material.STICK);
-        fireRodRecipe.setIngredient(' ', Material.SOUL_SAND);
         Bukkit.addRecipe(fireRodRecipe);
 
         // 伝説の弓
-        ShapedRecipe legendaryBowRecipe = new ShapedRecipe(legendaryBowKey, createLegendaryBow());
+        ItemStack legendaryBow = createLegendaryBow();
+        ShapedRecipe legendaryBowRecipe = new ShapedRecipe(new NamespacedKey(this, "legendary_bow"), legendaryBow);
         legendaryBowRecipe.shape(" p ", "aya", " f ");
         legendaryBowRecipe.setIngredient('p', Material.PAPER);
         legendaryBowRecipe.setIngredient('a', Material.ARROW);
@@ -98,14 +79,20 @@ public class OiwaraeruPlugin extends JavaPlugin implements Listener {
         legendaryBowRecipe.setIngredient('f', Material.FLINT_AND_STEEL);
         Bukkit.addRecipe(legendaryBowRecipe);
 
-        // 他のレシピも同様に登録してください
+        // 伝説の剣
+        ItemStack legendarySword = createLegendarySword();
+        ShapedRecipe legendarySwordRecipe = new ShapedRecipe(new NamespacedKey(this, "legendary_sword"), legendarySword);
+        legendarySwordRecipe.shape(" n ", "sss", " s ");
+        legendarySwordRecipe.setIngredient('n', Material.NETHERITE_SWORD);
+        legendarySwordRecipe.setIngredient('s', Material.GOLD_INGOT);
+        Bukkit.addRecipe(legendarySwordRecipe);
     }
 
     private ItemStack createVampireSword() {
         ItemStack item = new ItemStack(Material.IRON_SWORD);
         ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.DARK_RED + "吸血鬼の剣");
-        meta.setLore(Collections.singletonList(ChatColor.RED + "攻撃した分の50%を吸収する"));
+        meta.setDisplayName(VAMPIRE_SWORD_NAME);
+        meta.setLore(Collections.singletonList(ChatColor.RED + "攻撃ダメージの50%を吸収する"));
         item.setItemMeta(meta);
         return item;
     }
@@ -113,8 +100,8 @@ public class OiwaraeruPlugin extends JavaPlugin implements Listener {
     private ItemStack createIceRod() {
         ItemStack item = new ItemStack(Material.BRINE_ROD);
         ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.AQUA + "氷の杖");
-        meta.setLore(Collections.singletonList(ChatColor.BLUE + "ブロックリーチ50m、移動速度低下付与"));
+        meta.setDisplayName(ICE_ROD_NAME);
+        meta.setLore(Collections.singletonList(ChatColor.BLUE + "ブロックリーチ50m 移動速度低下付与"));
         item.setItemMeta(meta);
         return item;
     }
@@ -122,8 +109,8 @@ public class OiwaraeruPlugin extends JavaPlugin implements Listener {
     private ItemStack createFireRod() {
         ItemStack item = new ItemStack(Material.BLAZE_ROD);
         ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.RED + "炎の杖");
-        meta.setLore(Collections.singletonList(ChatColor.GOLD + "周囲のmobに炎上付与"));
+        meta.setDisplayName(FIRE_ROD_NAME);
+        meta.setLore(Collections.singletonList(ChatColor.GOLD + "周囲mobに炎上付与"));
         item.setItemMeta(meta);
         return item;
     }
@@ -131,29 +118,26 @@ public class OiwaraeruPlugin extends JavaPlugin implements Listener {
     private ItemStack createLegendaryBow() {
         ItemStack item = new ItemStack(Material.BOW);
         ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.GOLD + "伝説の弓");
-        meta.setLore(Collections.singletonList(ChatColor.YELLOW + "チャージで最大750ダメージ"));
+        meta.setDisplayName(LEGENDARY_BOW_NAME);
+        meta.setLore(Collections.singletonList(ChatColor.YELLOW + "チャージ最大750ダメージ"));
         item.setItemMeta(meta);
         return item;
     }
 
-    // ここに残りのアイテム生成メソッドも同様に実装してください
-
-    @EventHandler
-    public void onCraft(CraftItemEvent event) {
-        // 隠しアイテムクラフト監視、レシピ削除、全体チャット表示などの処理をここに実装
+    private ItemStack createLegendarySword() {
+        ItemStack item = new ItemStack(Material.NETHERITE_SWORD);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(LEGENDARY_SWORD_NAME);
+        meta.setLore(Arrays.asList(
+            ChatColor.GREEN + "移動速度上昇5",
+            ChatColor.GREEN + "攻撃力上昇3",
+            ChatColor.GREEN + "ジャンプ力上昇2",
+            ChatColor.GREEN + "回復速度上昇5"
+        ));
+        item.setItemMeta(meta);
+        return item;
     }
 
-    @EventHandler
-    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-        // 吸血鬼の剣、伝説の剣、炎の杖などの特殊効果処理をここに実装
-    }
-
-    @EventHandler
-    public void onEntityResurrect(EntityResurrectEvent event) {
-        // 不死のトーテム（隠しトーテム）優先処理をここに実装
-    }
-
-    // 他にも必要なイベント・メソッドを実装してください
+    // ここから実際の効果処理（イベントハンドラなど）を追加していきます...
 
 }
